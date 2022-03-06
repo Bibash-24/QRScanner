7b950010114374c7584e0e8970524c2eed024bbb
Bibash-24 <bbus24.a@gmail.com>
2022-03-06 16:39:03 +0545

    change

--- lib/scan.dart
+++ lib/scan.dart
@@ -1,46 +1,92 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:a2z_qr/Model/users.dart';
+import 'package:a2z_qr/Providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Scan extends StatefulWidget {
+  final User user;
+  const Scan({Key key, this.user}) : super(key: key);
   @override
   _ScanState createState() => _ScanState();
 }
 
 class _ScanState extends State<Scan> {
   String result = "press the camera to start the scan !";
+  String ticketNumberID;
+  String pinCode;
 
-  Future _scanQR() async {
-    try {
-      ScanResult qrScanResult = await BarcodeScanner.scan();
-      String qrResult = qrScanResult.rawContent;
-      setState(() {
-        result = qrResult;
-      });
-    } on PlatformException catch (ex) {
-      if (ex.code == BarcodeScanner.cameraAccessDenied) {
-        setState(() {
-          result = "Camera was denied";
-        });
-      } else {
-        setState(() {
-          result = "Unknown Error $ex";
-        });
-      }
-    } on FormatException {
-      setState(() {
-        result = "You pressed the back button before scanning anything";
-      });
-    } catch (ex) {
-      setState(() {
-        result = "Unknown Error $ex";
-      });
-    }
-  }
+  // Future _scanQR() async {
+  //   try {
+  //     ScanResult qrScanResult = await BarcodeScanner.scan();
+  //     String qrResult = qrScanResult.rawContent;
+  //     setState(() {
+  //       result = qrResult;
+  //     });
+  //     sendQRData(ticketNumberID, pinCode);
+  //   } on PlatformException catch (ex) {
+  //     if (ex.code == BarcodeScanner.cameraAccessDenied) {
+  //       setState(() {
+  //         result = "Camera was denied";
+  //       });
+  //     } else {
+  //       setState(() {
+  //         result = "Unknown Error $ex";
+  //       });
+  //     }
+  //   } on FormatException {
+  //     setState(() {
+  //       result = "You pressed the back button before scanning anything";
+  //     });
+  //   } catch (ex) {
+  //     setState(() {
+  //       result = "Unknown Error $ex";
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
+    User user = Provider.of<UserProvider>(context).user;
+
+    // Function to post QR data to Api
+    Future<void> sendQRData(ticketNumberID, pinCode) async {
+      Map<String, dynamic> jsonData = {
+        "mode": "checkedIn",
+        "data": {
+          "ticketNumberID": 'E015-NOR-6',
+          "pinCode": '313922',
+          "isDelevered": 0,
+          "isUsed": 0,
+          "isActive": 0
+        }
+      };
+
+      var response =
+          await http.post(Uri.parse("https://api.a2zticketing.com/api/checkIn"),
+              headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": user.token,
+              },
+              body: json.encode(jsonData));
+      // encoding: Encoding.getByName("utf-8"));
+
+      // var jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Data sent Sucessfully")));
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Error sending Data")));
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Scan QR"),
@@ -68,7 +114,9 @@ class _ScanState extends State<Scan> {
             FloatingActionButton.extended(
               icon: Icon(Icons.camera_alt),
               label: Text("Scan"),
-              onPressed: _scanQR,
+              onPressed: () {
+                sendQRData(ticketNumberID, pinCode);
+              },
             ),
           ],
         ),
